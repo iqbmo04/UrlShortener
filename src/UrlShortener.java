8e2c336ad236a65295b35928184b39227007cb22
rarcega <rarcega@gmail.com>
2012-08-24 02:41:41 -0300

    Update src/UrlShortener.java
    fixed source code formatting

--- src/UrlShortener.java
+++ src/UrlShortener.java
@@ -1,42 +1,42 @@
 public class UrlShortener
 {
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int    BASE     = 62;
+	private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	private static final int    BASE     = 62;
 
 	/**
-	 * Encodes a numeric key as a short alphanumeric value.
-	 *
-	 * @param   num the key to encode
-	 * @return  the alphanumeric value
-	 */
-    public static String encode(int num)
-    {
-        StringBuilder sb = new StringBuilder();
+	* Encodes a numeric key as a short alphanumeric value.
+	*
+	* @param   num the key to encode
+	* @return  the alphanumeric value
+	*/
+	public static String encode(int num)
+	{
+		StringBuilder sb = new StringBuilder();
 
-        while ( num > 0 )
-        {
-            sb.append( ALPHABET.charAt( num % BASE ) );
-            num /= BASE;
-        }
+		while ( num > 0 )
+		{
+			sb.append( ALPHABET.charAt( num % BASE ) );
+			num /= BASE;
+		}
 
-       return sb.reverse().toString();
-    }
+	return sb.reverse().toString();
+	}
 
 	/**
-	 * Decodes an alphanumeric value into its numeric key.
-	 *
-	 * @param   str the String to decode
-	 * @return  the decoded key
-	 */
+	* Decodes an alphanumeric value into its numeric key.
+	*
+	* @param   str the String to decode
+	* @return  the decoded key
+	*/
 	public static int decode(String str)
-    {
-        int num = 0;inty=0;
+	{
+		int num = 0;inty=0;
 
-        for ( int i = 0, len = str.length(); i < len; i++ )
-        {
-            num = num * BASE + ALPHABET.indexOf( str.charAt(i) );
-        }
+		for ( int i = 0, len = str.length(); i < len; i++ )
+		{
+			num = num * BASE + ALPHABET.indexOf( str.charAt(i) );
+		}
 
-        return num;
-    }
+		return num;
+	}
 }
