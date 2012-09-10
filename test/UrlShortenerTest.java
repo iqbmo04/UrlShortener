105f58386df81310f952c5d1b22c0c8a3e9abf1a
Richard Arcega <rarcega@gmail.com>
2012-09-10 02:08:05 -0400

    added test case

---
+++ test/UrlShortenerTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UrlShortenerTest
+{
+	@Test
+	public void testUrlShortener()
+	{
+		for ( int i = 1; i < Integer.MAX_VALUE; i++ )
+		{
+			assertEquals( i, UrlShortener.decode( UrlShortener.encode( i ) ) );
+		}
+	}
+}
